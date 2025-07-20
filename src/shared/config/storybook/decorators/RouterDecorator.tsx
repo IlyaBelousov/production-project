932d9e567912b4ce54da0b3afa87a2f6972d7db5
ilabelousov <ilya.belousov.95@list.ru>
2025-07-20 15:28:16 +0300

    add main pipeline

--- src/shared/config/storybook/decorators/RouterDecorator.tsx
+++ src/shared/config/storybook/decorators/RouterDecorator.tsx
@@ -1,14 +1,8 @@
 import '../../../../app/styles/index.scss';
 
-import { Meta, StoryObj } from '@storybook/react-webpack5';
-import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-const meta = {} satisfies Meta<ReactNode>;
-
-type Story = StoryObj<typeof meta>
-
-export const RouterDecorator = (story: () => Story) => (
+export const RouterDecorator = (story) => (
     <BrowserRouter>
         {story()}
     </BrowserRouter>

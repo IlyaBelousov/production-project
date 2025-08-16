990e57c12d6189eb3530816569a74d48e46c8713
ilabelousov <ilya.belousov.95@list.ru>
2025-08-16 14:16:58 +0300

    fix ts

--- src/shared/config/storybook/decorators/ThemeDecorator.tsx
+++ src/shared/config/storybook/decorators/ThemeDecorator.tsx
@@ -2,6 +2,7 @@ import { Theme } from 'app/providers/ThemeProvider';
 import { Decorator } from '@storybook/react'; // correct type from Storybook
 import React from 'react';
 
+// eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme): Decorator => (StoryFn) => (
     <div className={`app ${theme}`}>
         <StoryFn />
